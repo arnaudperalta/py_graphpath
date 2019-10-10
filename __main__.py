ae0d5d1c9afd6cba86e14ea97ae6720130ef7699
arnaudperalta <arnaud274@gmail.com>
2019-10-10 13:23:23 +0200

    cfg + matrice accessibilité

--- __main__.py
+++ __main__.py
@@ -4,9 +4,10 @@ from graph import Graph
 
 
 def run_project():
-    with open('./config.json', 'r') as fichier:
+    with open('./cfg/graph1.json', 'r') as fichier:
         data = json.load(fichier)
     graph = Graph(data)
+    graph.rdv_optimal()
 
 
 if __name__ == '__main__':
--- lib/graph.py
+++ lib/graph.py
@@ -1,10 +1,51 @@
+import numpy.matlib
+import numpy as np
+import sys
+numpy.set_printoptions(threshold=sys.maxsize)
+
+
 class Graph:
     def __init__(self, data):
-        print("nbNoeuds : " + str(data["nbNoeuds"]))
-        print("liste arcs : ")
-        for arc in data["arcs"]:
-            print(arc["sommetInitial"]
-                  + " -- "
-                  + str(arc["duree"])
-                  + " --> "
-                  + arc["sommetTerminal"])
+        self.size = data["nbNoeuds"]
+        self.sommetsList = list(data["nomSommets"])
+        self.rdvList = list(data["nomRdv"])
+        self.sommetsIniList = list(data["nomSommetsInitiaux"])
+        self.arcs = list(data["arcs"])
+
+        if self.sommetsList.__len__() != data["nbNoeuds"]:
+            print("Erreur dans le fichier configuration (nbNoeuds/nomSommets)\n")
+
+        if self.rdvList.__len__() != data["nbLieuxRdv"]:
+            print("Erreur dans le fichier configuration (nbLieuxRdv/nomRdv)\n")
+
+    def mat_graph(self):
+        mat = np.full((self.size, self.size), np.inf)
+        for arc in self.arcs:
+            mat[self.pos_sommet(arc["sommetInitial"])
+                , self.pos_sommet(arc["sommetTerminal"])] = arc["duree"]
+        return mat
+
+    def pos_sommet(self, char):
+        return self.sommetsList.index(char)
+
+    def transform(self, mat):
+        size_pair = self.size * self.size
+        mat_pair = np.full((size_pair , size_pair), np.inf)
+
+        for i in range(self.size):
+            for j in range(self.size):
+                for k in range(self.size):
+                    for l in range(self.size):
+                        if i == k and j == l :
+                            mat_pair[i * self.size + j, k * self.size + l] \
+                                = np.inf
+                        elif i == k :
+                            mat_pair[i * self.size + j, k * self.size + l] \
+                                = mat[j, l]
+                        elif j == l :
+                            mat_pair[i * self.size + j, k * self.size + l] \
+                                = mat[i, k]
+        print(mat_pair)
+
+    def rdv_optimal(self):
+        self.transform(self.mat_graph())
