5dcd5cdd6fc88935f3318fdfb86a40f0536282ef
LouisPariente <56361713+LouisPariente@users.noreply.github.com>
2019-10-11 22:20:25 +0200

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -8,28 +8,36 @@ def load_config(config):
         return json.load(fichier)
 
 
-def run_project():
-    graph = "graph1.json"
+def print_menu():
     print("1. Choisir la config (Defaut : graph1.json)")
     print("2. Lancer l'algorithme TempsDeParcoursMinimal")
     print("3. Lancer l'algorithme NombreDeCheminsMinimal\n")
     print("h pour acceder à l'aide")
     print("q pour Quitter")
+
+
+def print_menu_config():
+    print("Configurations disponibles :\n")
+    print("graph1.json")
+    print("graph_sujet.json")
+
+
+def run_project():
+    config = "graph1.json"
+    print_menu()
     ans = True
     while ans:
         ans = input("")
         if ans == "1":
-            print("Configurations disponibles :\n")
-            print("graph1.json")
-            print("graph_sujet.json")
-            graph = input("Configuration choisie?\n")
+            print_menu_config()
+            config = input("Configuration choisie?\n")
         elif ans == "2":
-            graph = Graph(load_config(graph))
-            graph.rdv_optimal()
+            config = Graph(load_config(config))
+            config.rdv_optimal()
             ans = None
         elif ans == "3":
-            graph = Graph(load_config(graph))
-            graph.rdv_optimal()
+            config = Graph(load_config(config))
+            config.rdv_optimal()
             ans = None
         elif ans == "h":
             print("AIDE A COMPLETER\n")
