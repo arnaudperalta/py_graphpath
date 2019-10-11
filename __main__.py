ca5afe8650713cb28ca392447900aa51be359835
LouisPariente <56361713+LouisPariente@users.noreply.github.com>
2019-10-11 17:40:48 +0200

    Add files via upload

--- __main__.py
+++ __main__.py
@@ -1,14 +1,46 @@
 import json
 
-from graph import Graph
+from lib.graph import Graph
+
+# Import the necessary packages
+
+#Le graph1 est celui utilisé par defaut
+graphe_name = 'graph1'
 
 
 def run_project():
-    with open('./cfg/graph1.json', 'r') as fichier:
+    with open('./cfg/' + graphe_name + '.json', 'r') as fichier:
         data = json.load(fichier)
     graph = Graph(data)
     graph.rdv_optimal()
 
 
 if __name__ == '__main__':
-    run_project()
+
+    ans = True
+    while ans:
+        print("""
+        1. Lancer le programme
+        2. Choix du graphe
+        3. Aide
+        4. Quitter
+        """)
+        ans = input("Que faire ?")
+        if ans == "1":
+            run_project()
+        elif ans == "2":
+            print("""
+            Graphes disponibles :
+            
+            Graph1
+            Graph_sujet
+            """)
+            ans2 = input("Nom du graphe?")
+            graphe_name = ans2
+        elif ans == "3":
+            print("\n AIDE A COMPLETER")
+        elif ans == "4":
+            print("\n Fin du programme")
+            ans = None
+        else:
+            print("\n Choix non valide, essayez autre chose")
