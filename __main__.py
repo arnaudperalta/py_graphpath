ba393c0e4fa594705cfcd3b57d2a7079987ba59c
arnaudperalta <arnaud274@gmail.com>
2019-10-14 16:16:49 +0200

    Correction main

--- __main__.py
+++ __main__.py
@@ -1,55 +1,100 @@
+# coding: utf-8
 import json
+import sys
 
-from lib.graph import Graph
+from graph import Graph
 from os import listdir
 from os.path import isfile, join
 
+CONFIG_DEFAUT = "graph1.json"
 
+
+# Fonction main du projet
+def run_project():
+    config = CONFIG_DEFAUT
+    # Affichage du menu principal
+    print_menu(config)
+    while True:
+        s = raw_input()
+        for c in s:
+            if c == "c":
+                # Affichage du menu de configuration avec recupération du fichier
+                config = menu_config()
+            elif c == "t" or c == "n":
+                # Création de l'objet Graph avec en paramètre la configuration choisie
+                graph = Graph(load_config(config))
+                # Execution du premier algo
+                start_algo(graph, c)
+            elif c == "h":
+                # Affichage de l'aide
+                print_help()
+            elif c == "q":
+                # Fin du programme
+                break
+        print_menu(config)
+
+
+# Fonction d'affichage de l'aide
+def print_menu(config):
+    print("py_graphpath")
+    print("-----------------------------------------------------")
+    print("c : Choisir la configuration (Actuel : " + config + ")")
+    print("t : Lancer l'algorithme Point de rendez-vous optimal par le temps")
+    print("n : Lancer l'algorithme Point de rendez-vous optimal par les déplacements")
+    print("h : pour acceder à l'aide")
+    print("q : spour arrêter le programme.")
+
+
+# Fonction du système de configuration du programme
+def menu_config():
+    config = ""
+    print("Configurations disponibles :")
+    list_cfg = [f for f in listdir('./cfg') if isfile(join('./cfg', f))]
+    for i in range(0, len(list_cfg)):
+        print(list_cfg[i])
+    while True:
+        config = raw_input("").rstrip()
+        if list_cfg.__contains__(config):
+            print("Configuration changée avec succès.")
+            break
+        else:
+            print("Cette configuration n'existe pas.")
+    return config
+
+
+# Fonction d'affichage de l'aide
+def print_help():
+    print("AIDE A COMPLETER\n")
+
+
+# Fonction de lancement des deux algorithmes du programme
+def start_algo(g, c):
+    if g.error != 0:
+        if g.error == 1:
+            print("Erreur dans le fichier configuration (nbNoeuds/nomSommets)\n")
+        elif g.error == 2:
+            print("Erreur dans le fichier configuration (nbLieuxRdv/nomRdv)\n")
+    if c == "t":
+        resultat = g.rdv_optimal()
+        if resultat != "":
+            print ("Le point de rendez-vous le plus optimal par le temps est : " + resultat)
+        else:
+            print("Pas de point de rendez-vous compatible")
+    elif c == "n":
+        resultat = g.rdv_optimal2()
+        if resultat != "":
+            print ("Le point de rendez-vous le plus optimal par les déplacements est : " + resultat)
+        else:
+            print("Pas de point de rendez-vous compatible")
+    print("\n")
+
+
+# Fonction du chargement de la configuration JSON en mémoire
 def load_config(config):
     with open('./cfg/' + config, 'r') as fichier:
         return json.load(fichier)
 
 
-def print_menu():
-    print("1. Choisir la config (Defaut : graph1.json)")
-    print("2. Lancer l'algorithme TempsDeParcoursMinimal")
-    print("3. Lancer l'algorithme NombreDeCheminsMinimal\n")
-    print("h pour acceder à l'aide")
-    print("q pour Quitter")
-
-
-def print_menu_config():
-    print("Configurations disponibles :\n")
-    list_cfg = [f for f in listdir('./cfg') if isfile(join('./cfg', f))]
-    for i in range(0, len(list_cfg)):
-        print(list_cfg[i])
-
-
-def run_project():
-    config = "graph1.json"
-    print_menu()
-    ans = True
-    while ans:
-        ans = input("")
-        if ans == "1":
-            print_menu_config()
-            config = input("Configuration choisie?\n")
-        elif ans == "2":
-            config = Graph(load_config(config))
-            config.rdv_optimal()
-            ans = None
-        elif ans == "3":
-            config = Graph(load_config(config))
-            config.rdv_optimal()
-            ans = None
-        elif ans == "h":
-            print("AIDE A COMPLETER\n")
-        elif ans == "q":
-            print("Fin du programme\n")
-            ans = None
-        else:
-            print("Commande non reconnue\n")
-
-
+# Lancement du main
 if __name__ == '__main__':
     run_project()
--- lib/graph.py
+++ lib/graph.py
@@ -11,12 +11,14 @@ class Graph:
         self.rdvList = list(data["nomRdv"])
         self.sommetsIniList = list(data["nomSommetsInitiaux"])
         self.arcs = list(data["arcs"])
-
+        self.error = 0
         if self.sommetsList.__len__() != data["nbNoeuds"]:
-            print("Erreur dans le fichier configuration (nbNoeuds/nomSommets)\n")
-
+            self.error = 1
         if self.rdvList.__len__() != data["nbLieuxRdv"]:
-            print("Erreur dans le fichier configuration (nbLieuxRdv/nomRdv)\n")
+            self.error = 2
+
+    def error(self):
+        return self.error
 
     def mat_graph(self):
         mat = np.full((self.size, self.size), np.inf)
@@ -30,7 +32,7 @@ class Graph:
 
     def transform(self, mat):
         size_pair = self.size * self.size
-        mat_pair = np.full((size_pair , size_pair), np.inf)
+        mat_pair = np.full((size_pair, size_pair), np.inf)
 
         for i in range(self.size):
             for j in range(self.size):
@@ -49,34 +51,32 @@ class Graph:
 
     def rdv_optimal(self):
         mat_pcd = self.mat_pcd(self.transform(self.mat_graph()))
-        init = self.pos_sommet(self.sommetsIniList[0])*self.size + self.pos_sommet(self.sommetsIniList[1])
+        init = self.pos_sommet(self.sommetsIniList[0]) * self.size + self.pos_sommet(self.sommetsIniList[1])
         rdv = []
         for c in self.rdvList:
-            rdv.append(self.pos_sommet(c)*self.size + self.pos_sommet(c))
+            rdv.append(self.pos_sommet(c) * self.size + self.pos_sommet(c))
         res = np.inf
         fin = np.inf
         for i in range(len(rdv)):
-            if mat_pcd[init,rdv[i]] < res:
-                res = mat_pcd[init,rdv[i]]
+            if mat_pcd[init, rdv[i]] < res:
+                res = mat_pcd[init, rdv[i]]
                 fin = i
             if mat_pcd[init, rdv[i]] != np.inf:
                 print("sommet : " + self.rdvList[i])
                 print("distance : " + str(mat_pcd[init, rdv[i]]))
         if fin != np.inf:
-            print("le rdv le plus optimal est : " + self.rdvList[fin])
+            return str(self.rdvList[fin])
         else:
-            print("pas de rdv possible")
-
-
+            return ""
 
     def mat_pcd(self, mat):
         size_pair = self.size * self.size
         for i in range(size_pair):
-            mat[i,i] = 0
+            mat[i, i] = 0
         for k in range(size_pair):
             for i in range(size_pair):
                 for j in range(size_pair):
-                    mat[i,j] = min(mat[i,j],mat[i,k]+mat[k,j])
+                    mat[i, j] = min(mat[i, j], mat[i, k]+mat[k, j])
         return mat
 
     def mat_pcc(self, mat):
@@ -86,53 +86,54 @@ class Graph:
         matpcc = np.full((size_pair, size_pair), 0)
         for i in range(size_pair):
             for j in range(size_pair):
-                if i != j and mat[i,j] != np.inf:
-                    matpcc[i,j] = i + 1;
+                if i != j and mat[i, j] != np.inf:
+                    matpcc[i, j] = i + 1
         for k in range(size_pair):
             for i in range(size_pair):
                 for j in range(size_pair):
-                    av = mat[i,j]
-                    mat[i,j] = min(mat[i,j],mat[i,k]+mat[k,j])
-                    if av != mat[i,j] :
+                    av = mat[i, j]
+                    mat[i, j] = min(mat[i, j], mat[i, k]+mat[k, j])
+                    if av != mat[i, j]:
                         matpcc[i, j] = k + 1
-        return (mat, matpcc)
+        return mat, matpcc
 
     def rdv_optimal2(self):
         doublemat = self.mat_pcc(self.transform(self.mat_graph()))
-        init = self.pos_sommet(self.sommetsIniList[0])*self.size + self.pos_sommet(self.sommetsIniList[1])
+        init = self.pos_sommet(self.sommetsIniList[0]) * self.size + self.pos_sommet(self.sommetsIniList[1])
         rdv = []
         for c in self.rdvList:
-            rdv.append(self.pos_sommet(c)*self.size + self.pos_sommet(c))
+            rdv.append(self.pos_sommet(c) * self.size + self.pos_sommet(c))
         res = []
         res2 = []
         for i in range(len(rdv)):
             k = 0
             pos = i
-            while doublemat[1][init,pos] != 0 and k < (self.size**2):
-                pos = doublemat[1][init,pos] - 1
+            while doublemat[1][init, pos] != 0 and k < (self.size ** 2):
+                pos = doublemat[1][init, pos] - 1
                 ++k
-            if pos == init :
+            if pos == init:
                 res.append(k)
-            else :
+            else:
                 res.append(np.inf)
-            res2.append(doublemat[0][init,rdv[i]])
-        min = np.inf
+            res2.append(doublemat[0][init, rdv[i]])
+        minimum = np.inf
         candidat = []
         for i in range(len(res)):
-            if res[i] < min:
+            if res[i] < minimum:
                 candidat.clear()
                 candidat.append(i)
-            elif res[i] == min :
+            elif res[i] == minimum:
                 candidat.append(i)
 
         resfinal = candidat[0]
-        if(len(candidat) > 1):
+        if len(candidat) > 1:
             minfinal = np.inf
             for k in range(len(candidat)):
                 if res2[candidat[k]] < minfinal:
                     resfinal = candidat[k]
                     minfinal = res2[candidat[k]]
         if resfinal != np.inf:
-            print("le rdv le plus optimal est : " + self.rdvList[resfinal])
+            return str(self.rdvList[resfinal])
         else:
-            print("pas de rdv possible")
+            return ""
+
